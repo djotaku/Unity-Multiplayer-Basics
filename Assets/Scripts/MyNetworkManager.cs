46c99cbd2778e9f28e8ee45e82b66304d1804f17
Eric Mesa <ericsbinaryworld@gmail.com>
2020-11-09 18:26:50 -0500

    playing around with network manager
    Learning how to add custom functionality when clients connect

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        Debug.Log("I connected to a server.");
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        Debug.Log($"There are now {numPlayers} players.");
+    }
+}
