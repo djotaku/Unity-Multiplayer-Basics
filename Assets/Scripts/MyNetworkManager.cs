8a01b22e5c432db6e1aa4db1612d5f848c58d3c7
Eric Mesa <ericsbinaryworld@gmail.com>
2020-11-12 19:13:26 -0500

    Created syncvars for player name and color

--- Assets/Scripts/MyNetworkManager.cs
+++ Assets/Scripts/MyNetworkManager.cs
@@ -5,17 +5,15 @@ using Mirror;
 
 public class MyNetworkManager : NetworkManager
 {
-    public override void OnClientConnect(NetworkConnection conn)
-    {
-        base.OnClientConnect(conn);
-
-        Debug.Log("I connected to a server.");
-    }
-
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
         base.OnServerAddPlayer(conn);
 
-        Debug.Log($"There are now {numPlayers} players.");
+        MyNetworkPlayer player = conn.identity.GetComponent<MyNetworkPlayer>();
+
+        player.SetDisplayName($"Player {numPlayers}");
+
+        player.SetDisplayColour(new Color(Random.Range(0f,1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+
     }
 }
---
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkPlayer : NetworkBehaviour
+{
+    [SyncVar]
+    [SerializeField]
+    private string displayName = "Missing Name";
+
+    [SyncVar]
+    [SerializeField]
+    private Color displayColour = Color.black;
+
+    [Server]
+    public void SetDisplayColour(Color newDisplayColour)
+    {
+        displayColour = newDisplayColour;
+    }
+
+
+    [Server]
+    public void SetDisplayName(string newDisplayName)
+    {
+        displayName = newDisplayName;
+    }
+}
