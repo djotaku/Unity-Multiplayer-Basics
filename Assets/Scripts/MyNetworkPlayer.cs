89dfa60ddca761cba4079c038978554e1aeb27f4
Eric Mesa <ericsbinaryworld@gmail.com>
2020-11-24 15:22:26 -0500

    Section 8 - Server Authority

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -34,6 +34,8 @@ public class MyNetworkPlayer : NetworkBehaviour
     [Command]
     private void CmdSetDisplayName(string newDisplayName)
     {
+        if(newDisplayName.Length < 2 || newDisplayName.Length > 20) { return; }
+
         RpcLogNewName(newDisplayName);
         
         SetDisplayName(newDisplayName);
@@ -57,7 +59,7 @@ public class MyNetworkPlayer : NetworkBehaviour
     [ContextMenu("Set My Name")]
     private void SetMyName()
     {
-        CmdSetDisplayName("My New Name");
+        CmdSetDisplayName("M");
     }
 
     [ClientRpc]
